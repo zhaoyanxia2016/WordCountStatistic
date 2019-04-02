d7c2072e587d4b1b5777b16bf695a72c18eb63fa
zhoulinmu <48110262+zhoulinmu@users.noreply.github.com>
2019-04-02 16:52:43 +0800

    Update and rename function.java to PrintToFile.java

---
+++ WordCount/src/PrintToFile.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import javax.swing.JOptionPane;
+/**
+ * @author Lin Zhou,YanXiao Zhao
+ * @data 2019.3.31
+ */
+/**按字典顺序输出到文件*/
+public class PrintToFile 
+{	
+	Map<String,Integer> Map = new LinkedHashMap<String, Integer>(); 
+	 /**按字典顺序排序*/
+    void Sort(Map<String, Integer> map) 
+    {  
+       Set<Entry<String,Integer>> m= map.entrySet();   
+       LinkedList<Entry<String, Integer>> List = new LinkedList<Entry<String,Integer>>(m);
+    
+    	   Collections.sort(List, new Comparator<Entry<String,Integer>>() 
+    	   {     
+               public int compare(Entry<String, Integer> a,  Entry<String, Integer> b) 
+               {  
+                   return a.getKey().compareTo(b.getKey());  
+              }     
+           });  
+       for (Entry<String,Integer> entry: List) 
+       {  
+           Map.put(entry.getKey(), entry.getValue());  
+       }  
+   } 
+    /**写入文件*/
+	 void PrintToF(Map<String, Integer> map)throws IOException 
+	 {  
+		    long start = System.currentTimeMillis();
+	    	Sort(map);
+	        File file = new File("result.txt");
+	        FileWriter f = new FileWriter(file.getAbsoluteFile());
+	        for (Entry<String,Integer> w: Map.entrySet()) 
+	        {
+	        	f.write(w.getKey() + "/" + w.getValue()+"     ");
+	        }
+	        f.close();
+	        JOptionPane.showConfirmDialog(null,"所用时间："+(System.currentTimeMillis() - start)+"ms","结果",JOptionPane.DEFAULT_OPTION);
+	 }         
+}
